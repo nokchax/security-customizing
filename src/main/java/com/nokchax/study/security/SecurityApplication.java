ed3266987434b7845fcbd44b5ea2f11f54e3d91b
nokchax <nokchax@gmail.com>
2020-08-27 22:59:04 +0900

    comment about entry point

--- src/main/java/com/nokchax/study/security/SecurityApplication.java
+++ src/main/java/com/nokchax/study/security/SecurityApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityApplication {
 
+    /*
+     * 시작점 @SpringBootApplication
+     * -> AutoConfigurationPackages
+     * -> SpringBootWebSecurityConfiguration
+     * -> WebSecurityConfigurerAdapter // 여기서 설정 시작
+     */
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
     }
