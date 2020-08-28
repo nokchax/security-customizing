6f90a1134ab0451c42c856c8d52d23e6bd0a5c29
nokchax <nokchax@gmail.com>
2020-08-28 23:22:08 +0900

    WebSecurityConfigurerAdapter

--- src/main/java/com/nokchax/study/security/SecurityApplication.java
+++ src/main/java/com/nokchax/study/security/SecurityApplication.java
@@ -8,9 +8,13 @@ public class SecurityApplication {
 
     /*
      * 시작점 @SpringBootApplication
+     * -> EnableAutoConfiguration
      * -> AutoConfigurationPackages
      * -> SpringBootWebSecurityConfiguration
      * -> WebSecurityConfigurerAdapter // 여기서 설정 시작
+     *      -> 시큐리티를 커스터마이징 할때도 이 클래스를 상속하는 방식을 이용함
+     *      -> WebSecurityConfiguration 가 하는 일은???
+     * -> 
      */
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
