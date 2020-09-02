9b3d995d77946d71e285d1d7770fa767bd8a0d0f
nokchax <nokchax@gmail.com>
2020-09-02 22:16:39 +0900

    ApplicationFilterChain 에 대하여...

--- src/main/java/com/nokchax/study/security/SecurityApplication.java
+++ src/main/java/com/nokchax/study/security/SecurityApplication.java
@@ -17,7 +17,17 @@ public class SecurityApplication {
      * -> WebSecurityConfigurerAdapter // 여기서 설정 시작
      *      -> 시큐리티를 커스터마이징 할때도 이 클래스를 상속하는 방식을 이용함
      *      -> WebSecurityConfiguration 가 하는 일은???
+     *      -> Congifuration을 가지고 SpringSecurityFilterChain을 생성한다 이 객체의 타입은 Filter
+     *      -> webSecurityExpressionHandler에서 
      * -> 
+     */
+
+    /*
+     * StandardWrapperValve (org.apache.catalina.core)
+     *  - ApplicationFilterChain 을 가지고 있음
+     *  - dispatch 전에 위의 필터 체인을 실행
+     *  - dispatch 넌 어느 시점에 들어가는가..?
+     *
      */
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
