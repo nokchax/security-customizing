7eb9fdc84ad50ccc11cd6e03d57a8fb37c479e0b
nokchax <nokchax@gmail.com>
2020-09-26 22:33:49 +0900

    feat: add twitter oauth1 request token that contains requestToken and oauthVerifier

---
+++ src/main/java/com/nokchax/study/security/twitter/TwitterOauth1RequestToken.java
@@ -0,0 +1,6 @@
+package com.nokchax.study.security.twitter;
+
+public class TwitterOauth1RequestToken {
+    // token that contains requestToken and oauthVerifier that need to authenticate
+    // this token is used before load user info
+}
