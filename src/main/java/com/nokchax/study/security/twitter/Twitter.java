5c9f073f674167b95ab0dd8a96c0e90ac7cb3b73
nokchax <nokchax@gmail.com>
2020-09-26 22:36:09 +0900

    feat: add new token for specific purpose

--- src/main/java/com/nokchax/study/security/twitter/Twitter.java
+++ src/main/java/com/nokchax/study/security/twitter/Twitter.java
@@ -1,4 +1,5 @@
 package com.nokchax.study.security.twitter;
 
 public class Twitter {
+    // 사용자 정보를 담는 객체, authentication의 detail에 들어가는 정보가 된다.
 }
--- src/main/java/com/nokchax/study/security/twitter/TwitterRedirectFilter.java
+++ src/main/java/com/nokchax/study/security/twitter/TwitterRedirectFilter.java
@@ -1,4 +1,6 @@
 package com.nokchax.study.security.twitter;
 
 public class TwitterRedirectFilter {
+    // twitter redirect 시에 callback url 을 설정해서 요청 보낼 수 있다.
+    // RequestToken 은 저장해 둬야 한다.
 }
