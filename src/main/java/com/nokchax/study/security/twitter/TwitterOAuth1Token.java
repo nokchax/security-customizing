9c49d1bf78c3ec9e7fdec0b46f8d9ec3e8d87428
nokchax <nokchax@gmail.com>
2020-10-09 21:54:20 +0900

    feat: add twitter authentication object

--- src/main/java/com/nokchax/study/security/twitter/TwitterOAuth1Token.java
+++ src/main/java/com/nokchax/study/security/twitter/TwitterOAuth1Token.java
@@ -1,5 +1,24 @@
 package com.nokchax.study.security.twitter;
 
-public class TwitterOAuth1Token {
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class TwitterOAuth1Token extends AbstractAuthenticationToken {
+
+    public TwitterOAuth1Token(Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
     // 사용자 정보를 로드한 이후에 사용되는 토큰
 }
