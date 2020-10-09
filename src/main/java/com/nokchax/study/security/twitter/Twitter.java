aac467564a1c8b777b95980ede3e9c2f7fb7a245
nokchax <nokchax@gmail.com>
2020-10-09 21:54:39 +0900

    feat: add twitter principal object

--- src/main/java/com/nokchax/study/security/twitter/Twitter.java
+++ src/main/java/com/nokchax/study/security/twitter/Twitter.java
@@ -1,5 +1,25 @@
 package com.nokchax.study.security.twitter;
 
-public class Twitter {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Twitter implements OAuth2User {
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
     // 사용자 정보를 담는 객체, authentication의 detail에 들어가는 정보가 된다.
 }
