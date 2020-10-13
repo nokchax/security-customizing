d4019e95a85df0ec0e4d6dbd5720d04a2eb017b9
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2020-10-13 23:03:37 +0900

    201013

--- src/main/java/com/nokchax/study/security/twitter/TwitterAuthenticationProvider.java
+++ src/main/java/com/nokchax/study/security/twitter/TwitterAuthenticationProvider.java
@@ -4,6 +4,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
+// twitter accessToken 과 secretToken 을 가지고 twitter api를 호출하여 사용자 정보를 가지고오는 역할을 한다.
 public class TwitterAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
