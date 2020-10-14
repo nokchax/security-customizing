13fb6256bf94a3c7d5015c8435ea20afe5320512
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2020-10-14 21:00:11 +0900

    comment: about twitter redirect filter

--- src/main/java/com/nokchax/study/security/twitter/TwitterRedirectFilter.java
+++ src/main/java/com/nokchax/study/security/twitter/TwitterRedirectFilter.java
@@ -1,5 +1,6 @@
 package com.nokchax.study.security.twitter;
 
+// twitter 로그인에 필요한 accessToken 과 tokenSecret 을 얻기 위해서 트위터 쪽으로 요청을 보내기 위한 필터
 public class TwitterRedirectFilter {
     // twitter redirect 시에 callback url 을 설정해서 요청 보낼 수 있다.
     // RequestToken 은 저장해 둬야 한다.
