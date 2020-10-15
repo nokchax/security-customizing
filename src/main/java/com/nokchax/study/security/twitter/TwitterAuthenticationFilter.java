a08b08b972fa4483e5945f123371e3a994c957a7
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2020-10-15 23:13:29 +0900

    comment: authentication processing filter for twit

--- src/main/java/com/nokchax/study/security/twitter/TwitterAuthenticationFilter.java
+++ src/main/java/com/nokchax/study/security/twitter/TwitterAuthenticationFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 트위터 로그인 정보로 만든 인증 요청 토큰을 가지고 시큐리티에서 실제로 활용하는 인증 토큰을 생성하는 필터
 public class TwitterAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
     protected TwitterAuthenticationFilter(String defaultFilterProcessesUrl) {
         super(defaultFilterProcessesUrl);
